bea463b777ff58879672b4f129019f18ece1cd50
steinar <steinar1993@hotmail.com>
2019-02-09 21:23:53 +0100

    STATUS: GUI.py works. mpi_halo_exchange.py is still work in progress.
    CAenvironment.py:
        added option to initiate CAenvironment without providing x and y
        for source area.
    
    mpi_halo_exchange.py:
        work in progress. Added method to convert between global grid indices
        and local grid indices. Started working on method for adding source
        tiles to local grids.
    
    Signed-off-by: steinar <steinar1993@hotmail.com>

--- kladd.py
+++ kladd.py
@@ -1,15 +1,22 @@
-from configparser import ConfigParser, ExtendedInterpolation
+
+
 import numpy as np
 
-parser = ConfigParser(interpolation=ExtendedInterpolation())
-parser.read('test.ini')
-sections = parser.sections()
+def global_coords_to_local_coords(y, x, my_mpi_row, my_mpi_col, p_local_grid_x_dim, p_local_grid_y_dim):
+    ''' Convert between global and local indices. Return (-1,-1) if outside local grid.'''
+    if (x >= (my_mpi_col * p_local_grid_x_dim)) & (x < ((my_mpi_col + 1) * p_local_grid_x_dim)):
+        local_x = x - (my_mpi_col * p_local_grid_x_dim)
+    else:
+        return tuple([-1, -1])
+    if (y >= (my_mpi_row * p_local_grid_y_dim)) & (y < ((my_mpi_row + 1) * p_local_grid_y_dim)):
+        local_y = y - (my_mpi_row * p_local_grid_y_dim)
+    else:
+        return tuple([-1, -1])
+    return tuple([local_y,local_x])
 
-items = parser.items(sections[0])
+# a = global_coords_to_local_coords(100,99, 1,0,100,100)
 
-parameters = {}
-for i in range(len(items)):
-    try:
-        parameters[items[i][0]] = eval(items[i][1])
-    except:
-        parameters[items[i][0]] = (items[i][1])
+test_coords_x = np.array([0,0])
+test_coords_y = np.array([0,100])
+
+b = global_coords_to_local_coords(test_coords_y, test_coords_x, 0,0,100,100)
