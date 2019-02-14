58dcdb75bfe6c0941b438a3bf65048f2a5a89ad0
steinar <steinar1993@hotmail.com>
2019-02-14 16:00:29 +0100

    Started working on border exchange function for cubes. Need to implement "corner exchange" because of the stencil used in the CA.
    Signed-off-by: steinar <steinar1993@hotmail.com>

--- mpi_halo_exchange.py
+++ mpi_halo_exchange.py
@@ -3,120 +3,125 @@ import numpy as np
 import sys
 import CAenvironment as CAenv
 
+def exchange_borders_cube(local_petri_A, z_dim):
 
-
-def exchange_borders():
-    if (num_iterations + 1) % 2:
-
+    for i in range(z_dim):
         # Send data south and receive from north
+        local_grid_send_south = local_petri_A[-2,:,i].copy()
+        local_grid_receive_north = np.zeros(p_local_grid_x_dim+2, dtype=np.double, order='C')
         comm.Sendrecv(
-            [local_petri_A[-2, :], 1, border_row_t],  # send the second last row
+            [local_grid_send_south, p_local_grid_x_dim + 2, MPI.DOUBLE],  # send the second last row
             neighbor_processes[DOWN],
             0,
-            [local_petri_A[0, :], 1, border_row_t],  # recvbuf = first row
+            [local_grid_receive_north, p_local_grid_x_dim + 2, MPI.DOUBLE],  # recvbuf = first row
             neighbor_processes[UP],
             0
         )
+        local_petri_A[0,:,i] = local_grid_receive_north.copy()
 
         # # Send data north and receive from south
+        local_grid_send_north = local_petri_A[1,:,i].copy()
+        local_grid_receive_south = np.zeros(p_local_grid_x_dim + 2, dtype=np.double, order='C')
         comm.Sendrecv(
-            [local_petri_A[1, :], 1, border_row_t],  # sendbuf = second row (1.row = border)
+            [local_grid_send_north, p_local_grid_x_dim + 2, MPI.DOUBLE],  # sendbuf = second row (1.row = border)
             neighbor_processes[UP],  # destination
             1,  # sendtag
-            [local_petri_A[-1, :], 1, border_row_t],  # recvbuf = last row
+            [local_grid_receive_south, p_local_grid_x_dim +2, MPI.DOUBLE],  # recvbuf = last row
             neighbor_processes[DOWN],  # source
             1
         )
-        #
+        local_petri_A[-1,:,i] = local_grid_receive_south.copy()
+
         # Send west and receive from east
-        local_grid_ev[:] = local_petri_A[:, 1].copy()
+        local_grid_send_west = local_petri_A[:, 1, i].copy()
+        local_grid_receive_east = np.zeros(p_local_grid_y_dim + 2, dtype=np.double, order='C')
         comm.Sendrecv(
-            [local_grid_ev, p_local_grid_y_dim + 2, MPI.DOUBLE],
+            [local_grid_send_west, p_local_grid_y_dim + 2, MPI.DOUBLE],
             neighbor_processes[LEFT],
             2,
-            [local_grid_wb, p_local_grid_y_dim + 2, MPI.DOUBLE],
+            [local_grid_receive_east, p_local_grid_y_dim + 2, MPI.DOUBLE],
             neighbor_processes[RIGHT],
             2
         )
-        local_petri_A[:, -1] = local_grid_wb.copy()
+        local_petri_A[:, -1, i] = local_grid_receive_east.copy()
 
-        # # Send east and receive from west
-        local_grid_wv[:] = local_petri_A[:, -2].copy()
-        # print("my value = ", local_grid_wv)
-        # print("my destination = ", local_grid_eb)
-
+        # Send east and receive from west
+        local_grid_send_east = local_petri_A[:, -2, i].copy()
+        local_grid_receive_west = np.zeros(p_local_grid_y_dim + 2, dtype=np.double, order='C')
         comm.Sendrecv(
-            [local_grid_wv, p_local_grid_y_dim + 2, MPI.DOUBLE],
+            [local_grid_send_east, p_local_grid_y_dim + 2, MPI.DOUBLE],
             neighbor_processes[RIGHT],
             0,
-            [local_grid_eb, p_local_grid_y_dim + 2, MPI.DOUBLE],
+            [local_grid_receive_west, p_local_grid_y_dim + 2, MPI.DOUBLE],
             neighbor_processes[LEFT],
             0
         )
-        local_petri_A[:, 0] = local_grid_eb.copy()
-    else:
+        local_petri_A[:, 0, i] = local_grid_receive_west.copy()
 
-        # Send data south and receive from north
-        comm.Sendrecv(
-            [local_petri_B[-2, :], 1, border_row_t],  # send the second last row
-            neighbor_processes[DOWN],
-            0,
-            [local_petri_B[0, :], 1, border_row_t],  # recvbuf = first row
-            neighbor_processes[UP],
-            0
-        )
 
-        # # Send data north and receive from south
-        comm.Sendrecv(
-            [local_petri_B[1, :], 1, border_row_t],  # sendbuf = second row (1.row = border)
-            neighbor_processes[UP],  # destination
-            1,  # sendtag
-            [local_petri_B[-1, :], 1, border_row_t],  # recvbuf = last row
-            neighbor_processes[DOWN],  # source
-            1
-        )
-        #
-        # Send west and receive from east
-        local_grid_ev[:] = local_petri_B[:, 1].copy()
-        comm.Sendrecv(
-            [local_grid_ev, p_local_grid_y_dim + 2, MPI.DOUBLE],
-            neighbor_processes[LEFT],
-            2,
-            [local_grid_wb, p_local_grid_y_dim + 2, MPI.DOUBLE],
-            neighbor_processes[RIGHT],
-            2
-        )
-        local_petri_B[:, -1] = local_grid_wb.copy()
+def exchange_borders_matrix(local_petri_A):
 
-        # # Send east and receive from west
-        local_grid_wv[:] = local_petri_B[:, -2].copy()
-        # print("my value = ", local_grid_wv)
-        # print("my destination = ", local_grid_eb)
 
-        comm.Sendrecv(
-            [local_grid_wv, p_local_grid_y_dim + 2, MPI.DOUBLE],
-            neighbor_processes[RIGHT],
-            0,
-            [local_grid_eb, p_local_grid_y_dim + 2, MPI.DOUBLE],
-            neighbor_processes[LEFT],
-            0
-        )
-        local_petri_B[:, 0] = local_grid_eb.copy()
+    # Send data south and receive from north
+    comm.Sendrecv(
+        [local_petri_A[-2, :], 1, border_row_t],  # send the second last row
+        neighbor_processes[DOWN],
+        0,
+        [local_petri_A[0, :], 1, border_row_t],  # recvbuf = first row
+        neighbor_processes[UP],
+        0
+    )
+
+    # # Send data north and receive from south
+    comm.Sendrecv(
+        [local_petri_A[1, :], 1, border_row_t],  # sendbuf = second row (1.row = border)
+        neighbor_processes[UP],  # destination
+        1,  # sendtag
+        [local_petri_A[-1, :], 1, border_row_t],  # recvbuf = last row
+        neighbor_processes[DOWN],  # source
+        1
+    )
+    #
+    # Send west and receive from east
+    local_grid_ev[:] = local_petri_A[:, 1].copy()
+    comm.Sendrecv(
+        [local_grid_ev, p_local_grid_y_dim + 2, MPI.DOUBLE],
+        neighbor_processes[LEFT],
+        2,
+        [local_grid_wb, p_local_grid_y_dim + 2, MPI.DOUBLE],
+        neighbor_processes[RIGHT],
+        2
+    )
+    local_petri_A[:, -1] = local_grid_wb.copy()
+
+    # # Send east and receive from west
+    local_grid_wv[:] = local_petri_A[:, -2].copy()
+    # print("my value = ", local_grid_wv)
+    # print("my destination = ", local_grid_eb)
+
+    comm.Sendrecv(
+        [local_grid_wv, p_local_grid_y_dim + 2, MPI.DOUBLE],
+        neighbor_processes[RIGHT],
+        0,
+        [local_grid_eb, p_local_grid_y_dim + 2, MPI.DOUBLE],
+        neighbor_processes[LEFT],
+        0
+    )
+    local_petri_A[:, 0] = local_grid_eb.copy()
+
 
 
 def iterateCA():
     pass
 
 
-def gather_grid():
+def gather_grid(local_petri_A):
     send = np.zeros((p_local_grid_y_dim, p_local_grid_x_dim), dtype=np.double)
     TEMP = np.zeros((p_local_grid_y_dim * p_y_dims * p_local_grid_x_dim * p_x_dims), dtype=np.double)
     IMAGE = np.zeros((p_local_grid_y_dim * p_y_dims * p_local_grid_x_dim * p_x_dims), dtype=np.double)
 
-    if ((num_iterations + 1) % 2 == 0):
-        send[:, :] = local_petri_B[1:-1, 1:-1]
-    else:
-        send[:, :] = local_petri_A[1:-1, 1:-1]
+
+    send[:, :] = local_petri_A[1:-1, 1:-1]
 
     comm.Gather(send, TEMP, 0)
     # if rank == 0: print('TEMP = \n', TEMP.reshape(p_local_grid_y_dim*p_y_dims,p_local_grid_x_dim*p_x_dims))
@@ -249,8 +254,10 @@ if __name__ == "__main__":
     DOWN = 1
     LEFT = 2
     RIGHT = 3
+    NORTH_EAST = 4
+    SOUTH_WEST = 5
 
-    neighbor_processes = [0, 0, 0, 0]
+    neighbor_processes = [0, 0, 0, 0, 0, 0]
 
 
 
@@ -279,6 +286,7 @@ if __name__ == "__main__":
 
     neighbor_processes[UP], neighbor_processes[DOWN] = cartesian_communicator.Shift(0, 1)
     neighbor_processes[LEFT], neighbor_processes[RIGHT] = cartesian_communicator.Shift(1, 1)
+    neighbor_processes[NORTH_EAST], neighbor_processes[SOUTH_WEST] = cartesian_communicator.Shift(direction=(1,1), disp=1)
 
 
 
@@ -307,15 +315,15 @@ if __name__ == "__main__":
 
 
 
-    local_petri_A = np.zeros((p_local_grid_x_dim + 2, p_local_grid_y_dim + 2))
-    local_petri_B = np.zeros((p_local_grid_x_dim + 2, p_local_grid_y_dim + 2))
+    # local_petri_A = np.zeros((p_local_grid_x_dim + 2, p_local_grid_y_dim + 2))
+
     local_grid_wb = np.zeros((p_local_grid_y_dim + 2), dtype=np.double, order='C')
     local_grid_eb = np.zeros((p_local_grid_y_dim + 2), dtype=np.double, order='C')
     local_grid_ev = np.zeros((p_local_grid_y_dim + 2), dtype=np.double, order='C')
     local_grid_wv = np.zeros((p_local_grid_y_dim + 2), dtype=np.double, order='C')
-    TEMP = np.empty((1), dtype=np.double, order='C')
 
-    local_petri_A[:] = comm.Get_rank()
+    p_local_hexgrid.grid.Q_o[:,:,1] = my_rank
+    # local_petri_A[:] = comm.Get_rank()
     # print("process = ", comm.rank,"\n",
     #       local_grid_A)
 
@@ -330,12 +338,21 @@ if __name__ == "__main__":
     border_col_t.Commit()
 
     for num_iterations in range(ITERATIONS):
-        exchange_borders()
+        exchange_borders_matrix(p_local_hexgrid.grid.Q_th)
+        exchange_borders_matrix(p_local_hexgrid.grid.Q_v)
+        for j in range(parameters['nj']):
+            exchange_borders_matrix(p_local_hexgrid.grid.Q_cj[:, :, j])
+            exchange_borders_matrix(p_local_hexgrid.grid.Q_cbj[:, :, j])
+        exchange_borders_matrix(p_local_hexgrid.grid.Q_d)
+        exchange_borders_matrix(p_local_hexgrid.grid.Q_a)
+        exchange_borders_cube(p_local_hexgrid.grid.Q_o, 6)
         iterateCA()
 
     comm.barrier()
-    IMAGE = gather_grid()
-    if my_rank == 0: print (IMAGE)
+    IMAGE_Q_th = gather_grid(p_local_hexgrid.grid.Q_th)
+    test = p_local_hexgrid.grid.Q_o[:,:,1].copy()
+    IMAGE_Q_o = gather_grid(test)
+    if my_rank == 0: print (IMAGE_Q_o)
 
-    # print("after \nprocess = ", comm.rank,"\n",
-    #       local_grid_A)
+    print("after \nprocess = ", my_rank,"\n",
+          p_local_hexgrid.grid.Q_o[:,:,1])
