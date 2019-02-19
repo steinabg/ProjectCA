Fixed bug where x and y coord was swtiched. (error was made by receiving local_x and local_y in wrong order from the converter function) Moved some functions outside main. Set maximum depth for rupert inlet bathymetry = 3. Started the channel at 0 instead of 1.
Signed-off-by: steinar <steinar1993@hotmail.com>

CAenvironment.py
@@ -2,6 +2,7 @@ from hexgrid import *
 import matplotlib.pyplot as plt
 import GUI
 # import sys
+import os.path
 # sys.path.append('..')
 np.set_printoptions(suppress=True, precision=3)
 
@@ -80,7 +81,7 @@ class CAenvironment():
 
         self.ICstates = [self.Q_th, self.Q_v, self.Q_cj, self.Q_cbj, self.Q_d, self.Q_o]
 
-        self.grid = Hexgrid(self.Ny, self.Nx, ICstates=self.ICstates, reposeAngle=np.deg2rad(parameters['theta_r']), terrain=self.terrain)
+        self.grid = Hexgrid(self.Ny, self.Nx, ICstates=self.ICstates, reposeAngle=np.deg2rad(parameters['theta_r']), terrain=self.terrain, global_grid=global_grid)
 
         self.grid.g = parameters['g']  # Gravitational acceleration
         self.grid.f = parameters['f']  # Darcy-Weisbach coeff
@@ -104,6 +105,9 @@ class CAenvironment():
         self.density = []
         self.beddensity = []
         self.head_velocity = []
+
+
+        self.save_path = './Data/'
 
         # For plotting in center of channel
         # self.ch_bot_thickness =
@@ -141,7 +145,7 @@ class CAenvironment():
         ax[3].set_title('Q_d[1:-1,1:-1]')
         plt.tight_layout()
         s1 = str(self.terrain) if self.terrain is None else self.terrain
-        plt.savefig('full_%03ix%03i_%s_%03i_thetar%0.0f.png' % (self.Nx, self.Ny, s1, i + 1, self.parameters['theta_r']),
+        plt.savefig(os.path.join('./Data/','full_%03ix%03i_%s_%03i_thetar%0.0f.png' % (self.Nx, self.Ny, s1, i + 1, self.parameters['theta_r']) ),
                     bbox_inches='tight', pad_inches=0, dpi=240)
         plt.close('all')
 
@@ -184,7 +188,7 @@ class CAenvironment():
         ax[2].set_ylim([0, 2])
         ax[2].set_ylabel('sum(Q_{o}[y,x])')
         plt.tight_layout()
-        plt.savefig('ch_bot_%03i.png' % (i + 1), bbox_inches='tight', pad_inches=0, dpi=240)
+        plt.savefig('./Data/ch_bot_%03i.png' % (i + 1), bbox_inches='tight', pad_inches=0, dpi=240)
 
 
         plt.close('all')

hexgrid.py
@@ -59,7 +59,7 @@ class Hexgrid():
             self.Q_d = np.ones((self.Ny, self.Nx)) * np.inf  # Thickness of soft sediment
             self.Q_d[1:-1, 1:-1] = 0
         else:
-            self.Q_d = np.zeros(self.Ny,self.Nx)
+            self.Q_d = np.zeros((self.Ny,self.Nx))
         if global_grid == True:
             self.Q_a = self.Q_d.copy()  # Bathymetry legges til Q_a i self.setBathymetry(terrain)
         # print(self.Q_a)

mathfunk.py
@@ -2,6 +2,7 @@ import numpy as np
 import warnings
 
 def generate_rupert_inlet_bathymetry(repose_angle, Ny=200, Nx=200, channel_amplitude=None, channel_width=None, channeldepth=None):
+    # TODO Make bathymetry with varying slope. 12 deg at top according to article
     # Nx = Ny = 200
     X = np.zeros((Nx, Ny))
 
@@ -17,16 +18,21 @@ def generate_rupert_inlet_bathymetry(repose_angle, Ny=200, Nx=200, channel_ampli
     if channel_width is None:
         channel_width = int(np.round(Nx / 6.667))
     if channeldepth is None:
-        channeldepth = int(np.round(Nx / 66.666))
+        channeldepth = np.minimum(int(np.round(Nx / 66.666)),3)
     cross_sectionY = channeldepth * np.sin(np.linspace(np.pi, 2 * np.pi, channel_width))
+    # def x_func(arg):
+    #     return arg*arg
+    # cross_sectionY = channeldepth * x_func(np.linspace(-1, 1, channel_width))- channeldepth
     for i in range(len(indicesY)):
         for j in range(channel_width):
             X[int(indicesY[i] - channel_width + j), int(indicesX[i])] = cross_sectionY[j]
 
     for j in range(channel_width):
-        X[int(np.round(Nx / 2) + y_offset - channel_width + j), np.arange(1, np.round(Ny / 3) + 1, dtype=np.int)] = \
+        X[int(np.round(Nx / 2) + y_offset - channel_width + j), np.arange(0, np.round(Ny / 3) + 1, dtype=np.int)] = \
         cross_sectionY[j]
 
+    # cross_sectionX = np.zeros((Ny))
+    # cross_sectionX = np.exp(-np.linspace(0,2,Ny))
     cross_sectionX = np.tan(np.deg2rad(-5)) * np.arange(1, Ny + 1)
 
     for i in range(Ny):

mpi_halo_exchange.py
@@ -188,7 +188,7 @@ def global_coords_to_local_coords(y, x, my_mpi_row, my_mpi_col, p_local_grid_x_d
         local_y = y - (my_mpi_row * p_local_grid_y_dim)
     else:
         return tuple([-1, -1])
-    return tuple([local_y, local_x])
+    return tuple([local_x, local_y])
 
 def set_local_grid_source_xy():
     p_local_source_tiles = []
@@ -252,9 +252,50 @@ def set_local_grid_source_xy():
     if len(p_local_source_tiles) > 0:
         p_local_grid_parameters['x'] = np.ix_(np.arange(np.min(p_local_source_tile_x),np.max(p_local_source_tile_x)+1))
         p_local_grid_parameters['y'] = np.ix_(np.arange(np.min(p_local_source_tile_y),np.max(p_local_source_tile_y)+1))
+        print("p_local_grid_parameters['x'] = {0}, p_local_grid_parameters['y'] = {1}".format(
+            p_local_grid_parameters['x'], p_local_grid_parameters['y']
+        ))
     # print(p_local_grid_parameters['x'])
     # print(p_local_grid_parameters['y'])
 
+def load_txt_files(num_iterations):
+    IMAGE_Q_th = np.loadtxt(os.path.join(save_path_txt, 'Q_th_{0}.txt'.format(num_iterations + 1)))
+    IMAGE_Q_cbj = np.loadtxt(os.path.join(save_path_txt, 'Q_cbj_{0}.txt'.format(num_iterations + 1)))
+    IMAGE_Q_cj = np.loadtxt(os.path.join(save_path_txt, 'Q_cj_{0}.txt'.format(num_iterations + 1)))
+    IMAGE_Q_d = np.loadtxt(os.path.join(save_path_txt, 'Q_d_{0}.txt'.format(num_iterations + 1)))
+    return IMAGE_Q_th, IMAGE_Q_cbj, IMAGE_Q_cj, IMAGE_Q_d
+
+def print_substate(Ny, Nx, i, Q_th, Q_cj, Q_cbj, Q_d, X0, X1, terrain):
+    fig = plt.figure(figsize=(10, 6))
+    ax = [fig.add_subplot(2, 2, i, aspect='equal') for i in range(1, 5)]
+    ind = np.unravel_index(np.argmax(Q_th, axis=None), Q_th.shape)
+
+    points = ax[0].scatter(X0.flatten(), X1.flatten(), marker='h',
+                           c=Q_cj[:, :].flatten())
+
+    plt.colorbar(points, shrink=0.6, ax=ax[0])
+    ax[0].set_title('Q_cj[:,:,0]. n = ' + str(i + 1))
+
+    points = ax[1].scatter(X0.flatten(), X1.flatten(), marker='h',
+                           c=Q_th.flatten())
+    # ax[1].scatter(X[ind[0],ind[1],0], X[ind[0],ind[1],1], c='r')  # Targeting
+    plt.colorbar(points, shrink=0.6, ax=ax[1])
+    ax[1].set_title('Q_th')
+
+    points = ax[2].scatter(X0[1:-1, 1:-1].flatten(), X1[1:-1, 1:-1].flatten(), marker='h',
+                           c=Q_cbj[1:-1, 1:-1].flatten())
+    plt.colorbar(points, shrink=0.6, ax=ax[2])
+    ax[2].set_title('Q_cbj[1:-1,1:-1,0]')
+
+    points = ax[3].scatter(X0[1:-1, 1:-1].flatten(), X1[1:-1, 1:-1].flatten(), marker='h',
+                           c=Q_d[1:-1, 1:-1].flatten())
+    plt.colorbar(points, shrink=0.6, ax=ax[3])
+    ax[3].set_title('Q_d[1:-1,1:-1]')
+    plt.tight_layout()
+    s1 = str(terrain) if terrain is None else terrain
+    plt.savefig(os.path.join(save_path_png,'full_%03ix%03i_%s_%03i_thetar%0.0f.png' % (Nx, Ny, s1, i + 1, parameters['theta_r'])),
+                bbox_inches='tight', pad_inches=0, dpi=240)
+    plt.close('all')
 
 if __name__ == "__main__":
     # Setup MPI
@@ -317,6 +358,7 @@ if __name__ == "__main__":
                                                                      Ny=parameters['ny'],
                                                                      Nx=parameters['nx'])
             global_bathy = np.transpose(global_bathy)
+            # global_bathy = np.ones((parameters['ny'],parameters['nx'])) # TODO: Remove after testing
             local_bathy = global_bathy[(my_mpi_row*p_local_grid_y_dim):((my_mpi_row+1)*p_local_grid_y_dim),
                                          my_mpi_col*p_local_grid_x_dim:((my_mpi_col+1)*p_local_grid_x_dim)]
             return local_bathy
@@ -381,8 +423,8 @@ if __name__ == "__main__":
     i_sample_values = []
 
     import os.path
-    save_path_txt = '/home/steinar/Dropbox/NTNU/ProjectCA/Data/mpi_combined_txt'
-    save_path_png = '/home/steinar/Dropbox/NTNU/ProjectCA/Data/mpi_combined_png'
+    save_path_txt = './Data/mpi_combined_txt'
+    save_path_png = './Data/mpi_combined_png'
     if my_rank == 0:
         np.savetxt(os.path.join(save_path_txt, 'X000.txt'), result_grid.grid.X[:, :, 0])
         np.savetxt(os.path.join(save_path_txt, 'X001.txt'), result_grid.grid.X[:, :, 1])
@@ -391,6 +433,28 @@ if __name__ == "__main__":
     q_cj0 = parameters['q_cj[y,x,0]']
     q_v0 = parameters['q_v[y,x]']
 
+    # Check how Q_a and Q_d looks
+    Image_Q_a = gather_grid(p_local_hexgrid.grid.Q_a)
+    Image_Q_d = gather_grid(p_local_hexgrid.grid.Q_d)
+    if my_rank == 0:
+        fig = plt.figure(figsize=(10, 6))
+        ax = [fig.add_subplot(1, 2, i, aspect='equal') for i in range(1, 3)]
+        # ind = np.unravel_index(np.argmax(Image_Q_a, axis=None), Image_Q_a.shape)
+
+        points = ax[0].scatter(result_grid.grid.X[:, :, 0].flatten(), result_grid.grid.X[:, :, 1].flatten(), marker='h',
+                               c=Image_Q_a[:, :].flatten())
+
+        plt.colorbar(points, shrink=0.6, ax=ax[0])
+        ax[0].set_title('Q_a[:,:]. ')
+        points = ax[1].scatter(result_grid.grid.X[:, :, 0].flatten(), result_grid.grid.X[:, :, 1].flatten(), marker='h',
+                               c=Image_Q_d[:, :].flatten())
+        plt.colorbar(points, shrink=0.6, ax=ax[1])
+
+        plt.savefig(os.path.join(save_path_png,'before.png'),bbox_inches='tight', pad_inches=0)
+    if my_rank== 0:
+        from timeit import default_timer as timer
+        start = timer()
+    # print("my_rank = {0}, parameters[x] = {1}, parameters[y] = {2}".format(my_rank,p_local_grid_parameters['x'],p_local_grid_parameters['y']))
     for num_iterations in range(ITERATIONS):
         # Add source
         p_local_hexgrid.addSource(q_th0,q_v0, q_cj0)
@@ -421,7 +485,28 @@ if __name__ == "__main__":
         # Iterate CA
         p_local_hexgrid.grid.time_step(global_grid=False)
 
+        # Debugging
+        if (num_iterations == 0):
+            # Check how Q_a and Q_d looks
+            Image_Q_a = gather_grid(p_local_hexgrid.grid.Q_a)
+            Image_Q_d = gather_grid(p_local_hexgrid.grid.Q_d)
+            if my_rank == 0:
+                fig = plt.figure(figsize=(10, 6))
+                ax = [fig.add_subplot(1, 2, i, aspect='equal') for i in range(1, 3)]
+                # ind = np.unravel_index(np.argmax(Image_Q_a, axis=None), Image_Q_a.shape)
 
+                points = ax[0].scatter(result_grid.grid.X[:, :, 0].flatten(), result_grid.grid.X[:, :, 1].flatten(),
+                                       marker='h',
+                                       c=Image_Q_a[:, :].flatten())
+
+                plt.colorbar(points, shrink=0.6, ax=ax[0])
+                ax[0].set_title('Q_a[:,:]. ')
+                points = ax[1].scatter(result_grid.grid.X[:, :, 0].flatten(), result_grid.grid.X[:, :, 1].flatten(),
+                                       marker='h',
+                                       c=Image_Q_d[:, :].flatten())
+                plt.colorbar(points, shrink=0.6, ax=ax[1])
+
+                plt.savefig(os.path.join(save_path_png,'after.png'), bbox_inches='tight', pad_inches=0)
 
         if ((num_iterations + 1) % sample_rate == 0) and num_iterations > 0:
             i_sample_values.append(num_iterations)
@@ -459,45 +544,6 @@ if __name__ == "__main__":
 
 
     # Print figures
-    def print_substate(Ny, Nx, i, Q_th, Q_cj, Q_cbj, Q_d, X0, X1, terrain):
-        fig = plt.figure(figsize=(10, 6))
-        ax = [fig.add_subplot(2, 2, i, aspect='equal') for i in range(1, 5)]
-        ind = np.unravel_index(np.argmax(Q_th, axis=None), Q_th.shape)
-
-        points = ax[0].scatter(X0.flatten(), X1.flatten(), marker='h',
-                               c=Q_cj[:, :].flatten())
-
-        plt.colorbar(points, shrink=0.6, ax=ax[0])
-        ax[0].set_title('Q_cj[:,:,0]. n = ' + str(i + 1))
-
-        points = ax[1].scatter(X0.flatten(), X1.flatten(), marker='h',
-                               c=Q_th.flatten())
-        # ax[1].scatter(X[ind[0],ind[1],0], X[ind[0],ind[1],1], c='r')  # Targeting
-        plt.colorbar(points, shrink=0.6, ax=ax[1])
-        ax[1].set_title('Q_th')
-
-        points = ax[2].scatter(X0[1:-1, 1:-1].flatten(), X1[1:-1, 1:-1].flatten(), marker='h',
-                               c=Q_cbj[1:-1, 1:-1].flatten())
-        plt.colorbar(points, shrink=0.6, ax=ax[2])
-        ax[2].set_title('Q_cbj[1:-1,1:-1,0]')
-
-        points = ax[3].scatter(X0[1:-1, 1:-1].flatten(), X1[1:-1, 1:-1].flatten(), marker='h',
-                               c=Q_d[1:-1, 1:-1].flatten())
-        plt.colorbar(points, shrink=0.6, ax=ax[3])
-        ax[3].set_title('Q_d[1:-1,1:-1]')
-        plt.tight_layout()
-        s1 = str(terrain) if terrain is None else terrain
-        plt.savefig(os.path.join(save_path_png,'full_%03ix%03i_%s_%03i_thetar%0.0f.png' % (Nx, Ny, s1, i + 1, parameters['theta_r'])),
-                    bbox_inches='tight', pad_inches=0, dpi=240)
-        plt.close('all')
-
-    def load_txt_files(num_iterations):
-        IMAGE_Q_th = np.loadtxt(os.path.join(save_path_txt, 'Q_th_{0}.txt'.format(num_iterations + 1)))
-        IMAGE_Q_cbj = np.loadtxt(os.path.join(save_path_txt, 'Q_cbj_{0}.txt'.format(num_iterations + 1)))
-        IMAGE_Q_cj = np.loadtxt(os.path.join(save_path_txt, 'Q_cj_{0}.txt'.format(num_iterations + 1)))
-        IMAGE_Q_d = np.loadtxt(os.path.join(save_path_txt, 'Q_d_{0}.txt'.format(num_iterations + 1)))
-        return IMAGE_Q_th, IMAGE_Q_cbj, IMAGE_Q_cj, IMAGE_Q_d
-
     num_figs = ITERATIONS//sample_rate
     figs_per_proc = num_figs//num_procs
     X0 = np.loadtxt(os.path.join(save_path_txt, 'X000.txt'))
@@ -510,7 +556,8 @@ if __name__ == "__main__":
                        X0, X1, parameters['terrain'])
 
 
-
+if my_rank == 0:
+    print("time = ", timer() - start)
 
 
     # if my_rank == 0: print(IMAGE_Q_o[:,:,0])